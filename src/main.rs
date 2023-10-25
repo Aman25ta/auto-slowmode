992aecb8e53863985034c93456b6d3c66148a5f0
Aman <aman25.ta@gmail.com>
2023-10-25 16:44:32 +0200

    rewrote most, have the main part left out

---
+++ src/main.rs
@@ -0,0 +1,159 @@
+#![warn(clippy::str_to_string)]
+
+mod commands;
+
+use poise::serenity_prelude as serenity;
+use std::{collections::HashMap, env::var, sync::Mutex, time::Duration};
+
+use std::sync::atomic::{AtomicU32, Ordering};
+use poise::Event;
+
+
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Custom user data passed to all command functions
+pub struct Data {
+    msgs: Mutex<HashMap<String, i32>>,
+}
+
+
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    // FrameworkOptions contains all of poise's configuration option in one struct
+    // Every option can be omitted to use its default value
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::help()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("~".into()),
+            edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+            additional_prefixes: vec![
+                poise::Prefix::Literal(">>"),
+                poise::Prefix::Literal("slow "),
+            ],
+            ..Default::default()
+        },
+        /// The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+        /// This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        /// This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        /// Every command invocation must pass this check to continue execution
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        /// Enforce command checks even for owners (enforced by default)
+        /// Set to true to bypass checks, which is useful for testing
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(event_handler(_ctx, event, _framework, _data))
+        },
+        ..Default::default()
+    };
+
+    poise::Framework::builder()
+        .token(
+            var("DISCORD_TOKEN")
+                .expect("Missing `DISCORD_TOKEN` env var, see README for more information."),
+        )
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as {}", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    msgs: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .run()
+        .await
+        .unwrap();
+
+
+
+        //thread for reset of the counter
+        
+
+}
+
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &Event<'_>,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
+) -> Result<(), Error> {
+    match event {
+        Event::Ready { data_about_bot } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        Event::Message { new_message } => {
+            let k = &(new_message.channel_id.to_string());
+
+            
+            let mut hash_map = data.msgs.lock().unwrap();
+            if hash_map.contains_key(&k.to_string()) {
+                let v = hash_map[k];
+                hash_map.insert(k.to_string(), 1 + v );
+            }
+
+        }
+        _ => {}
+    }
+    Ok(())
+}
+
+/*
+
+collect message count for each server:channel in a dict(hashmap?)
+--- DONE
+
+reset every 30 seconds
+before the reset, set slowmode accordingly (DONT FORGET TO DO THE 0SECOND)
+
+ -> /toggle <channel> [add/remove it to a db AND init in counter]
+
+ at every start take all channels and add to hashmap (make global somehow?(with the Data struct on top possibly? check the examples))
+ then start counting and changing slowmode accordingly
+
+*/
